d81380bf094b2e5b18d59b5c4ff71f094534978c
JeonSuna <jsa020910@gmail.com>
2024-11-14 13:38:22 +0900

    ⚡️

---
+++ workspace/ch02-start/state/01/src/components/Message.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+
+let count = 0;
+export default function Message() {
+  const [msg, setMsg] = useState('');
+
+  const handleChange = (event) => {
+    const inputMsg = event.target.value;
+    setMsg(inputMsg);
+    count++;
+  };
+  return (
+    <div>
+      <input type="text" onChange={handleChange} />
+      <br />
+      <span>입력메세지:{msg} </span>
+      <br />
+      <span>입력 횟수: {count}</span>
+    </div>
+  );
+}
