1a0d70fb5902d2e0e3b7ec961ff2f8a49545af15
JeonSuna <jsa020910@gmail.com>
2024-11-14 15:50:33 +0900

    04준비

---
+++ workspace/ch02-start/state/04/src/components/EditAddress.jsx
@@ -0,0 +1,21 @@
+import { Fragment } from 'react';
+
+export default function EditAddress({ addressBook, handleAddressChange }) {
+  const list = addressBook.map((address) => {
+    return (
+      <Fragment key={address.id}>
+        <label htmlFor={address.id}>{address.name}</label>
+        <input
+          type="text"
+          id={address.id}
+          name={address.name}
+          value={address.value}
+          onChange={handleAddressChange}
+        />
+        <br />
+      </Fragment>
+    );
+  });
+
+  return list;
+}
