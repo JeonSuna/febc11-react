f5af799e8a6873424883c8c759d61782065c99e8
JeonSuna <jsa020910@gmail.com>
2024-12-02 14:03:05 +0900

    class module사용

---
+++ workspace/ch08-css/02-module/src/components/Button.jsx
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types';
+import styles from './Button.module.css';
+import classNames from 'classnames';
+
+Button.propTypes = {
+  children: PropTypes.string.isRequired,
+  type: PropTypes.string,
+  color: PropTypes.oneOf(['blue', 'red', 'yellow']), // 글자 색상 ,/글자 색상,배열 값 중에 하나만 와야한다는 뜻 (이 배열 중 꼭 하나야 함)
+  bg: PropTypes.oneOf(['blue', 'red', 'yellow', 'gray']), // 배경 색상
+  onClick: PropTypes.func,
+};
+
+export default function Button({
+  children,
+  type = 'button',
+  bg,
+  color,
+  onClick: clickHandler,
+}) {
+  // const colorStyle = `${styles.button} ${styles[`color-${bg}-${color}`]}`;
+  const colorStyle = classNames(styles.button, styles[`color-${bg}-${color}`]);
+  return (
+    <button className={colorStyle} type={type} onClick={clickHandler}>
+      {children}
+    </button>
+  );
+}
